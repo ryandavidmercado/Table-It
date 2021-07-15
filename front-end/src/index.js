2ae88687f97ead2797194e5232c40ac21b39e65b
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-15 13:08:00 -0400

    Install and initalize Chakra UI.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ColorModeScript } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ColorModeScript />
       <App />
     </Router>
   </React.StrictMode>,
