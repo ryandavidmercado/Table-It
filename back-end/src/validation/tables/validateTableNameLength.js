311918e4e20fd58293bbaa165c3f66d904a45b16
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-19 12:43:21 -0400

    Remove optional chaining for backwards compatibility with older versions of Node.

--- back-end/src/validation/tables/validateTableNameLength.js
+++ back-end/src/validation/tables/validateTableNameLength.js
@@ -1,10 +1,10 @@
 const validateTableNameLength = (req, res, next) => {
-  return req.body.data.table_name?.length <= 1
-    ? next({
+  return req.body.data.table_name && req.body.data.table_name.length > 1
+    ? next()
+    : next({
         status: 400,
         message: "table_name must be a string with at least two characters.",
-      })
-    : next();
+      });
 };
 
 module.exports = validateTableNameLength;
